6b1b665539acdd13f0c7df7072fdc7f9db41c6de
cristianAguirre <cristianmoto066@gmail.com>
2024-04-26 12:29:47 -0300

    Ux v1

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -5,14 +5,20 @@ import {Link} from 'react-router-dom'
 
 export const NavBar = () => {
   return ( 
-    <nav className='flex content-center  '>
-        <div className=" justify-self-center h-[100px]  cursor-pointer  ">
+    
+        <nav className="flex justify-between py-5   sm:flex h-[100px] sm:flex-none sm:py-5 ">
        
+           <div>
+           <Link className="text-white font-mono   text-xl px-6 text-opacity-45 hover:text-slate-50 sm:text-3xl " to="/">Home</Link>
+           </div>
+            <div>
+               <Link className="text-white font-mono   text-xl px-6 text-opacity-45 hover:text-slate-50 sm:text-3xl" to="/ProjectContainer">Projects</Link>
+            </div>
+           <div>
+             <Link className="text-white font-mono text-xl px-6  text-opacity-45  hover:text-slate-50 sm:text-3xl" to="/Skills">Skills</Link>
+           </div>
            
-        <Link className="text-white font-mono   text-xl px-6 text-opacity-45 hover:text-slate-50 sm:text-3xl " to="/">Home</Link>
-        <Link className="text-white font-mono   text-xl px-6 text-opacity-45 hover:text-slate-50 sm:text-3xl" to="/ProjectContainer">Projects</Link>
-            <Link className="text-white font-mono text-xl px-6  text-opacity-45  hover:text-slate-50 sm:text-3xl" to="/Skills">Skills</Link>
-        </div>
+       
  
    </nav>
   )
